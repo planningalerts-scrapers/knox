91b2bcd8f7de795eb2be4ddc943fe1bfdc30bb90
Matthew Landauer <mlandauer@gmail.com>
2019-05-21 03:08:16 +1000

    Get rid of some debugging output

--- scraper.rb
+++ scraper.rb
@@ -8,7 +8,6 @@ url = "#{base_url}enquirylists.aspx"
 agent = Mechanize.new
 
 first_page = agent.get url
-p first_page.title.strip
 first_page_form = first_page.forms.first
 first_page_form.radiobuttons.first.click
 summary_page = first_page_form.click_button
@@ -17,7 +16,6 @@ page_number = 2 # The next page number to move onto (we've already got page 1)
 
 das_data = []
 while summary_page
-  p summary_page.title.strip
   table = summary_page.root.at_css('.ContentPanel')
   headers = table.css('th').collect { |th| th.inner_text.strip }
 
