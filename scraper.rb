538271fff7302a6bbdd1f2907d28266596e882ee
Matthew Landauer <mlandauer@gmail.com>
2019-05-21 03:07:51 +1000

    Get rid of ssl settings

--- scraper.rb
+++ scraper.rb
@@ -5,9 +5,7 @@ require 'date'
 base_url = "https://eservices.knox.vic.gov.au/ePathway/Production/Web/generalenquiry/"
 url = "#{base_url}enquirylists.aspx"
 
-agent = Mechanize.new do |a|
-  a.verify_mode = OpenSSL::SSL::VERIFY_NONE
-end
+agent = Mechanize.new
 
 first_page = agent.get url
 p first_page.title.strip
