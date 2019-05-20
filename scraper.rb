b0f08949f13237bef4bb40310f8b39a8d1e4891d
Matthew Landauer <mlandauer@gmail.com>
2019-05-21 04:16:12 +1000

    Small refactor

--- scraper.rb
+++ scraper.rb
@@ -14,13 +14,24 @@ summary_page = scraper.pick_type_of_search(:advertising)
 
 page_number = 2 # The next page number to move onto (we've already got page 1)
 
-das_data = []
 while summary_page
   table = summary_page.root.at_css('.ContentPanel')
+
+
   headers = table.css('th').collect { |th| th.inner_text.strip }
 
-  das_data = das_data + table.css('.ContentPanel, .AlternateContentPanel').collect do |tr|
-    tr.css('td').collect { |td| td.inner_text.strip }
+  table.css('.ContentPanel, .AlternateContentPanel').each do |tr|
+    da_item = tr.css('td').collect { |td| td.inner_text.strip }
+    record = {
+      'council_reference' => da_item[headers.index('Application Number')],
+      # There is a direct link but you need a session to access it :(
+      'info_url' => url,
+      'description' => da_item[headers.index('Description')],
+      'date_received' => Date.strptime(da_item[headers.index('Date Lodged')], '%d/%m/%Y').to_s,
+      'address' => da_item[headers.index('Location')],
+      'date_scraped' => Date.today.to_s
+    }
+    ScraperWiki.save_sqlite(['council_reference'], record)
   end
 
   if summary_page.at('#ctl00_MainBodyContent_mPagingControl_nextPageHyperLink')
@@ -31,17 +42,4 @@ while summary_page
   else
     summary_page = nil
   end
-end
-
-das_data.each do |da_item|
-  record = {
-    'council_reference' => da_item[headers.index('Application Number')],
-    # There is a direct link but you need a session to access it :(
-    'info_url' => url,
-    'description' => da_item[headers.index('Description')],
-    'date_received' => Date.strptime(da_item[headers.index('Date Lodged')], '%d/%m/%Y').to_s,
-    'address' => da_item[headers.index('Location')],
-    'date_scraped' => Date.today.to_s
-  }
-  ScraperWiki.save_sqlite(['council_reference'], record)
 end
